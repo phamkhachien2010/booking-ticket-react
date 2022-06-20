28da0ebb1fae056a6f1472433f67be850bed3980
phamkhachien2010 <phamkhachien17@gmail.com>
2022-06-20 12:55:03 +0700

    create booking ticket movie app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { createBrowserHistory } from 'history'
 import { Switch } from 'react-router-dom';
 import { Router } from 'react-router-dom';
+import LoadingComponent from './components/loadingComponent/LoadingComponent';
+import Hompage from './pages/Homepage/Hompage';
+import Login from './pages/Sign/Login';
+import Register from './pages/Sign/Register';
+import HomeTemplate from './templates/HomeTemplates/HomeTemplate';
+import UserTemplates from './templates/UserTemplates/UserTemplates';
 
 
 export const history = createBrowserHistory();
@@ -8,8 +14,15 @@ export const history = createBrowserHistory();
 function App() {
   return (
     <Router history={history}>
+       <LoadingComponent />
       <Switch>
-        <h1>Hello Hien</h1>
+
+        <HomeTemplate path='/home' exact Component={Hompage} />
+
+        <UserTemplates path='/login' exact Component={Login} />
+        <UserTemplates path='/register' exact Component={Register} />
+
+        <HomeTemplate path='/' exact Component={Hompage} />
       </Switch>
     </Router>
   );
